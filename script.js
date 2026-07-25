7b618fae1659323956d9d063de850f0d4d4de43f
Quantum <anim-awuahm@outlook.com>
2026-07-25 14:55:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC6xuhcVQn2V34lAZ5q5PStnj_veFeiPU0",
+    authDomain: "the-password-guess.firebaseapp.com",
+    projectId: "the-password-guess",
+    storageBucket: "the-password-guess.firebasestorage.app",
+    messagingSenderId: "179947705882",
+    appId: "1:179947705882:web:8db9cd66d9237c1d50a3a3"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
 // ==========================================
 // 1. FIREBASE CONFIGURATION & INITIALIZATION
 // ==========================================
